0f8eb370e24d0f8b673f7f0b836f7109b2c08f41
pecuenban <pedrocuenca95@yahoo.es>
2020-05-09 21:18:24 +0200

    Tarjetas responsibe y oscuro con memoria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,35 @@
-import { Component, ElementRef } from '@angular/core';
-import { MatSliderModule } from '@angular/material/slider';
-import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, ElementRef } from "@angular/core";
+import { MatSliderModule } from "@angular/material/slider";
+import { OverlayContainer } from "@angular/cdk/overlay";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent {
   constructor(overlayContainer: OverlayContainer, private _yo: ElementRef) {
-  //  overlayContainer.getContainerElement().classList.add('packtil-dark-theme');
+    //  overlayContainer.getContainerElement().classList.add('packtil-dark-theme');
   }
 
-  dark:boolean;
-  ngOnInit(){
-    this.dark = false;
-    
+  dark: boolean;
+  ngOnInit() {
+    if (localStorage.getItem("dark") == "true") {
+      this.dark = true;
+      this._yo.nativeElement.classList.add("packtil-dark-theme");
+    } else {
+      this.dark = false;
+    }
   }
 
-  cambioTema(){
-this.dark = !this.dark;
-if(this.dark){
-    this._yo.nativeElement.classList.add("packtil-dark-theme");
-}else{
-    this._yo.nativeElement.classList.remove("packtil-dark-theme");
-}
+  cambioTema() {
+    this.dark = !this.dark;
+    if (this.dark) {
+      this._yo.nativeElement.classList.add("packtil-dark-theme");
+      localStorage.setItem("dark", "true");
+    } else {
+      this._yo.nativeElement.classList.remove("packtil-dark-theme");
+      localStorage.setItem("dark", "false");
+    }
   }
-  
 }
