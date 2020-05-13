4c52605e5f36ad3e63c2b3ae1294d13e8f0af6c4
pecuenban <pedrocuenca95@yahoo.es>
2020-05-13 20:41:26 +0200

    cambios de estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { OverlayContainer } from "@angular/cdk/overlay";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   constructor(overlayContainer: OverlayContainer, private _yo: ElementRef) {
