1a47b5d2a9cd911a7bb4e5c4aae7576a389215e6
pecuenban <pedrocuenca95@yahoo.es>
2020-05-13 20:56:38 +0200

    vuelta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { OverlayContainer } from "@angular/cdk/overlay";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   constructor(overlayContainer: OverlayContainer, private _yo: ElementRef) {
